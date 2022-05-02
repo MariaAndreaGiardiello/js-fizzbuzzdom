1bfda377c8f5637fe133251348123f71186af752
MariaAndreaGiardiello <giardiello.m.a@icloud.com>
2022-05-02 23:05:51 +0200

    Analisi richiesta

---
+++ js/main.js
@@ -0,0 +1,21 @@
+//Consegna:
+//Scrivi un programma che stampi in console i numeri da 1 a 100.
+//MILESTONE 1
+//Per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
+//Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+//MILESTONE 2
+//Dato un container nel DOM, appendi un elemento html con il numero o la stringa corretta.
+//MILESTONE 3
+//Applica uno stile differente a seconda del valore dell'indice per i multipli di 3, per i multipli di 5 e per i valori che sono sia multipli di 3 che di 5.
+
+// 1. inserire contaainer fino a 100
+// 2. creare una costante per lo stile 
+// 3. creare una variabile per il testo all'interno dei quadrati
+// 4. strutturare un for per numerazione fino a 100
+
+// 5. creare un if per stabilire come comportarsi in caso di numero multiplo di 3
+        //testo "Fizz"
+// 6. creare un if per stabilire come comportarsi in caso di numero multiplo di 5
+        //testo "Buzz"
+// 7. creare un if per stabilire come comportarsi in caso di numero multiplo di 3 e di 5
+        //testo "FizzBuzz"
